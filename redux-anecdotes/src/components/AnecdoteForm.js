6bedffcce28861475ad3777473a14857a12b2b28
PikkuHukka <olli.x.ojala@helsinki.fi>
2020-02-25 21:13:03 +0200

    6.3-6.8

---
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+
+const AnecdoteForm = () => {
+
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    console.log('content:', content)
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { newVote } from '../reducers/anecdoteReducer'
+
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(newVote(id))
+  }
+
+  const anecdotes = useSelector(state => state)
+
+  return (
+    anecdotes.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has{anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )
+  )
+}
+
+
+export default AnecdoteList
